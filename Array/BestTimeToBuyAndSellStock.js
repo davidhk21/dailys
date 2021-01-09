66384243a11838b14068dab25c0c69aeadd03c2d
David Kim <davidhk21@gmail.com>
2021-01-08 17:08:41 -0700

    completed in linear time

--- Array/BestTimeToBuyAndSellStock.js
+++ Array/BestTimeToBuyAndSellStock.js
@@ -16,3 +16,46 @@ Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
+
+// Input: array of numbers
+// Output: number => maximum profit
+// Constraints: none
+// Edge Cases:
+  // no profit => 0
+  // no prices => 0
+
+// ORIGINAL SOLUTION: O(n^2) Time Complexity
+var maxProfit = function(prices) {
+  // create a result variable set to 0
+  let result = 0;
+  // iterate through prices
+  for (let i = 0; i < prices.length; i++) {
+    // iterate from i + 1
+    for (let j = i + 1; j < prices.length; j++) {
+      let diff = prices[j] - prices[i]
+      // if j - i is greater than result
+      if (diff > result) {
+        // set result to new max profit
+        result = diff;
+      }
+    }
+  }
+  // return result
+  return result;
+};
+
+// NEW SOLUTION: O(n) Time Complexity
+var maxProfit = function(prices) {
+  // create a min and max variable
+  let min = Infinity;
+  let max = 0;
+  // iterate through prices
+  for (let i = 0; i < prices.length; i++) {
+    // replace min if current index is lower
+    min = Math.min(min, prices[i]);
+    // replace max if current index is higher
+    max = Math.max(max, prices[i] - min);
+  }
+  // return max - min
+  return max;
+};
