f8d19b894b8ec94190444a6a6e5d785f3577ad50
David Kim <davidhk21@gmail.com>
2021-01-29 14:19:42 -0700

    got rid of comments

--- Array/CombinationSum.js
+++ Array/CombinationSum.js
@@ -20,37 +20,28 @@ These are the only two combinations.
 // Edge Cases: none
 
 var combinationSum = function(candidates, target) {
-  // create a result array variable
   let result = [];
-  // create inner recursive function with inputs of idx, temp value, combination array
+
   const checkCombinations = (idx, temp, comboArr) => {
-    // if temp value + num at idx = target
     if (temp + candidates[idx] === target) {
-      // push combination array to result array
       result.push([...comboArr, candidates[idx]]);
-      // return
       return;
     }
-    // if temp value + num at idx < target
     if (temp + candidates[idx] < target) {
       let newComboArr = [...comboArr, candidates[idx]];
       let newTemp = temp + candidates[idx];
-      // while idx >= 0
       while (idx >= 0) {
-        // recurse with each int at and below idx with each idx, new temp value, and new combination array
         checkCombinations(idx, newTemp, newComboArr);
         idx--;
       }
     }
   }
 
-  // sort the array to ascending order
   candidates.sort((a, b) => a - b);
-  // iterate through the candidates from right to left
+
   for (let i = candidates.length - 1; i >= 0; i--) {
-    // invoke recursive function for each candidate with idx, 0, and empty array
     checkCombinations(i, 0, []);
   }
-  // return result array
+
   return result;
 };
