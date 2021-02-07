f0cb332dd2780fa39e857b0bb1e5bb36e88d29d6
David Kim <davidhk21@gmail.com>
2021-02-06 18:34:06 -0700

    O(n) Time & O(1) Space

---
+++ Tree/InvertBinaryTree.js
@@ -0,0 +1,26 @@
+/*
+Invert a binary tree.
+
+Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+*/
+
+// Time: O(n)
+// Space: O(1)
+var invertTree = function(root) {
+  if (!root) return root;
+  if (root.left === null && root.right === null) return root;
+
+  let temp = root.right;
+  root.right = root.left;
+  root.left = temp;
+
+  if (root.left) invertTree(root.left);
+  if (root.right) invertTree(root.right);
+
+  return root;
+};
