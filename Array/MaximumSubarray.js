bf3632a293586de2426b9431ac10f2c84fbb8834
David Kim <davidhk21@gmail.com>
2021-02-07 23:55:45 -0700

    O(n) Time & O(1) Space

--- Array/MaximumSubarray.js
+++ Array/MaximumSubarray.js
@@ -2,6 +2,21 @@
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 */
 
+// Time: O(n)
+// Space: O(1)
+var maxSubArray = function(nums) {
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > 0) {
+      nums[i] += nums[i - 1];
+    }
+    result = Math.max(nums[i], result);
+  }
+  return result;
+}
+
+// Time: O(n^2)
+// Space: O(1)
 var maxSubArray = function(nums) {
   if (nums.length === 0) return 0;
   if (nums.length === 1) return nums[0];
