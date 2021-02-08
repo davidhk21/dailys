1b12ad347f7e0e9c7d54bd8cbe5cf1247c380855
David Kim <davidhk21@gmail.com>
2021-02-08 16:50:32 -0700

    O(n) Time & O(1) Space

---
+++ Binary/MissingNumber.js
@@ -0,0 +1,28 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+*/
+
+// Time: O(n)
+// Space: O(1)
+var missingNumber = function(nums) {
+  let n = nums.length;
+  let expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+  for (let i = 0; i < nums.length; i++) actualSum += nums[i];
+  return expectedSum - actualSum;
+}
+
+// Time: O(n log n)
+// Space: O(1)
+var missingNumber = function(nums) {
+  let sorted = nums.sort((a, b) => a - b);
+  let check = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] !== check) {
+      return i;
+    }
+    check++;
+  }
+};
